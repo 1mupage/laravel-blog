593371d240eb4c9f1546395a3f506767b3e57bf5
1mu <shaytanov40@gmail.com>
2025-10-23 17:37:00 +0300

    blog one-to-many and many-to-many convention

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
-use App\Models\PostTag;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
@@ -22,7 +21,11 @@ class PostController extends Controller
 //        $tag = Tag::find(1);
 //        dd($tag->posts);
 
-        $posts = Post::all();
+        $post = Post::find(1);
+        $category = Category::find(1);
+        $tag = Tag::find(1);
+
+        dd($post->tags);
 
         return view('post/index', compact('posts'));
     }
